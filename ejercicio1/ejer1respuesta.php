ccb68d3050415c301b548eed533b557b204488c5
abril zacarias <83786610+abrilzacarias@users.noreply.github.com>
2022-02-23 15:46:12 -0300

    Add files via upload

--- ejercicio1/ejer1respuesta.php
+++ ejercicio1/ejer1respuesta.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="formulario">
 <?php
 $diametro = $_POST['diametro'];
 $altura = $_POST['altura'];
@@ -8,5 +19,9 @@ $volumen = $PI*($radio^2)*$altura;
 
     echo "<br>Los valores de altura son ". $altura;
     echo "<br>Los valores de diametro son ". $diametro;
-    echo "<br>El volumen del cilindro es de ". $volumen. " 2/m";
+    echo "<br>El volumen del cilindro es de ". $volumen. " m/2";
+    
 ?>
+</div>
+</body>
+</html>
--- ejercicio1/ejercicio1.php
+++ ejercicio1/ejercicio1.php
@@ -4,16 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css.css">
     <title>Document</title>
 </head>
 <body>
+    
+    <div class="formulario">
+    <p> Ingrese la altura y el diametro del cilindro en metros: </p> 
     <form name="formulario" method="post" action="ejer1respuesta.php">
-        <p> Ingrese la altura y el diametro del cilindro en metros: </p> 
+        <div class="altura">
         <b>altura:</b>
-        <input type="number" name="altura" value="" required="">
+        <input type="number" name="altura" value="" required="" class="inputs">
+        </div>
+        <br>
+        <div class="diametro">
         <b>diametro:</b>
-        <input type="number" name="diametro" value="" required="">
+        <input type="number" name="diametro" value="" required="" class="inputs"><br>
         <input type="submit" value="Enviar datos">
+        </div>
+        </div>
 </form>
 </body>
 </html>
