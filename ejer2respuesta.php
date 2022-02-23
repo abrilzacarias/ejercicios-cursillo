b86c4565f7ba8e112c04d5ade6352cffbc603e7d
abril zacarias <83786610+abrilzacarias@users.noreply.github.com>
2022-02-23 14:22:57 -0300

    Delete ejer2respuesta.php

--- ejer2respuesta.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "el precio de la tienda 1 es de ". $_POST['tienda1']; 
-echo "<br>el precio de la tienda 2 es de ". $_POST['tienda2']; 
-echo "<br>el precio de la tienda 3 es de ". $_POST['tienda3']; 
-
-$promedio = $_POST['tienda1']+$_POST['tienda2']+$_POST['tienda3']/3;
-
-echo "<br>el promedio es de: " .$promedio;
-?>
