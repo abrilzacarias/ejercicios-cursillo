45e5092eff4866dca87ab113b5b4a9d986c9f1ac
abril zacarias <83786610+abrilzacarias@users.noreply.github.com>
2022-02-23 16:15:38 -0300

    Add files via upload

--- ejercicio2/ejer2respuesta.php
+++ ejercicio2/ejer2respuesta.php
@@ -1,9 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="resolution" >
 <?php
-echo "el precio de la tienda 1 es de ". $_POST['tienda1']; 
-echo "<br>el precio de la tienda 2 es de ". $_POST['tienda2']; 
-echo "<br>el precio de la tienda 3 es de ". $_POST['tienda3']; 
+    echo "el precio de la tienda 1 es de ". $_POST['tienda1']; 
+    echo "<br>el precio de la tienda 2 es de ". $_POST['tienda2']; 
+    echo "<br>el precio de la tienda 3 es de ". $_POST['tienda3']; 
 
-$promedio = $_POST['tienda1']+$_POST['tienda2']+$_POST['tienda3']/3;
+    $promedio = $_POST['tienda1']+$_POST['tienda2']+$_POST['tienda3']/3;
 
-echo "<br>el promedio es de: " .$promedio;
+    echo "<br>el promedio es de: " .$promedio;
 ?>
+</div>
+</body>
+</html>
+
--- ejercicio2/ejercicio2.php
+++ ejercicio2/ejercicio2.php
@@ -4,16 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css.css">
     <title>Document</title>
 </head>
 <body>
     <h1>Actividad N2</h1>
-    <h2>Ingrese el precio del producto para sacar su promedio:</h2>
+    <div class="input">
+    <h3>Ingrese el precio del producto para sacar su promedio:</h3>
     <form method="post" action="ejer2respuesta.php">
     <p>Tienda 1</p><input name="tienda1" type="number" value="" required=""><br>
     <p>Tienda 2</p><input name="tienda2" type="number" value="" required=""><br>
     <p>Tienda 3</p><input name="tienda3" type="number" value="" required=""><br>
-    <input type="submit" value="enviar" required=""><br>
+    <div class="submit">
+    <input type="submit" value="enviar" required="" id="submit">
+    </div>
+    
     </form>
+    </div>
 </body>
 </html>
