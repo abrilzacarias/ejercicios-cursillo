db97e37c43a5008038f7429de75db14f976a7d75
abril zacarias <83786610+abrilzacarias@users.noreply.github.com>
2022-02-23 14:24:35 -0300

    Add files via upload

---
+++ ejercicio2/ejercicio2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Actividad N2</h1>
+    <h2>Ingrese el precio del producto para sacar su promedio:</h2>
+    <form method="post" action="ejer2respuesta.php">
+    <p>Tienda 1</p><input name="tienda1" type="number" value="" required=""><br>
+    <p>Tienda 2</p><input name="tienda2" type="number" value="" required=""><br>
+    <p>Tienda 3</p><input name="tienda3" type="number" value="" required=""><br>
+    <input type="submit" value="enviar" required=""><br>
+    </form>
+</body>
+</html>
